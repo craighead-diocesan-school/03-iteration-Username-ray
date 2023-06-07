379ee31cf46253a56c6b598065cb45bec7cac1dc
Username-ray <maedara96@craighead.school.nz>
2023-06-07 15:17:16 +1200

    Completed task that use while loop and count

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -4,16 +4,26 @@
 
 function liftOff() {
   let count = 10
- 
-  while (count > 0) {
+
+  while(count > 0) {
     alert(count + '...')
     count = count - 1
   }
-   
-  alert('Lift Off!')
+
+  alert('Lift off!')
 }
 
 function make1() {
-  // copy and paste your code here when you get to the Make step
-  
+  let maximum = Number(prompt('Enter the maximum: '))
+  alert('Enter the maximum: ' + maximum)
+  let increment = Number(prompt('Enter the increment: '))
+  alert('Enter the increment: ' + increment)
+  let count = 0
+ 
+  while (count <= maximum) {
+    alert(count)
+    count = count + increment
+  }
+   
+  alert('Done!')
 }
