db1c984c5f7582f70a7de823cb383d52b34b847a
Username-ray <maedara96@craighead.school.nz>
2023-06-23 12:09:29 +1200

    Using while loop to display Fibonacci Sequence up to the number wanted

--- code/5-ext-fibonacci.js
+++ code/5-ext-fibonacci.js
@@ -3,5 +3,16 @@
 // ####################################
 
 function fibonacci() {
-  // write your code here
+  let maximum = prompt('What number do you want the Fibonacci Sequence up to?')
+  let a = 0
+  alert(a)
+  let b = 1
+  alert(b)
+
+  while (maximum > b) {
+    a = a + b
+    alert(a)
+    b = b + a
+    alert(b)
+  }
 }
