19b4e8d99448ce822ba32713f16ceb61310c41e4
Username-ray <maedara96@craighead.school.nz>
2023-07-22 12:08:56 +1200

    Added comments to explain what the codes doing

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -5,13 +5,16 @@
 function liftOff() {
   let count = 10
   let blassOff = 0
+  // Defined the first number(count) and the limit number(blass off).
 
   while(count > blassOff) {
     alert(count + '...')
     count = count - 1
+    // Show number to decrease by one until the count reach the blass off.
   }
 
   alert('Lift off!')
+  // At last, show a message that means it's end.
 }
 
 function make1() {
@@ -19,12 +22,16 @@ function make1() {
   alert('Enter the maximum: ' + maximum)
   let increment = Number(prompt('Enter the increment: '))
   alert('Enter the increment: ' + increment)
+  // Ask the number want a maximum and a increment and define them as the maximum and the increment. At this time, those inputs are recognised as numbers. And Shoe the sentence that make sure the numbers.
   let count = 0
+  // Define count to 0.
  
   while (count <= maximum) {
     alert(count)
     count = count + increment
   }
+  // During the count is smaller or the same as maximum, loop the codes to show the count and calculate and redefine the count.
    
   alert('Done!')
+  // Show a message that means it's end.
 }
--- code/2-attendees.js
+++ code/2-attendees.js
@@ -6,27 +6,34 @@ function attendees() {
   let count = 0
   let numOfAttendees = 5
   let attendeeList = ''
+  // Define the three valiables.
   
   while (count < numOfAttendees) {
     let attendee = prompt('Who is in attendance today?')
     attendeeList = attendeeList + attendee
     attendeeList = attendeeList + ', '
     count = count + 1
+    // During the count is smalleer than the number of attendees, loop that asking the attendee and define it as an attendees, and redefine the attendees list that includes the attendee who was inputed and the count counts up by one.
   }
   
-  alert(attendeeList) 
+  alert(attendeeList)
+  // Show the ultimate of the attendees list.
 }
 
 function make2() {
-  let numOfNumbers = prompt('How many numbers do you wanna add?')
+  let numOfNumbers = Number(prompt('How many numbers do you wanna add?'))
+  // Ask the number want to add and define it as a number of numbers.
   
   let count = 0
   let total = 0
+  // Define both the count and total number to 0.
   while (count < numOfNumbers) {
-    let whatever = prompt('Nek number;')
+    let whatever = prompt('Nek number: ')
     total = total + whatever
     count = count + 1
+    // During the count is smaller than the number of numbers, loop the codes that asks the Nek number and defines it as a whatever and redefines the total, and the count counts up by one.
   }
 
-  alert('total' + total)
+  alert('total: ' + total)
+  // At last, show the total number.
 }
